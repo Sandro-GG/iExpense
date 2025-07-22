2552ed5a7ea45dfa5a824a55818db6aa573474fb
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-22 12:05:53 +0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sandro Gakharia on 22.07.25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by Sandro Gakharia on 22.07.25.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
